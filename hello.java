b5f0d8eb3af536eba2afd3ffcba32856785118bf
hzy2017 <1394831854@qq.com>
2017-01-03 23:49:45 +0800

    第三次改版

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello{
 	public static void main(String[] args){
 	System.out.println("世界你好~");
 	System.out.println("Hello world");
+	System.out.println("区分工作区和暂存区区别");
+	System.out.println("今天2017年1月3号 23：48，我学会了创建仓库");
 }
 }
